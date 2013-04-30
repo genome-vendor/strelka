5700e7a471ef07413cc42f904782f90374c46855
apregier <aregier@genome.wustl.edu>
2013-04-30 15:18:37 -0500

    Squashed commit of the following:
    commit fbd0b2fc5676a8c4966811ff41afcfc90e5b01cf
    Author: apregier <aregier@genome.wustl.edu>
    Date:   Tue Apr 30 14:49:08 2013 -0500
    
        master branch is upstream only
    
    commit d3fdea86ef25b4b0a8836e7a0be9c841efcf8d89
    Author: apregier <aregier@genome.wustl.edu>
    Date:   Mon Jan 14 15:40:13 2013 -0600
    
        Remove - from package name
    
    commit d033ae996cf0b9feec254f30f162fa1ebad7482c
    Author: apregier <aregier@genome.wustl.edu>
    Date:   Wed Jan 9 13:09:40 2013 -0600
    
        Update version in debian files
    
    commit 952df79f8972ae44fc83eff92810d27ab6f4dd20
    Author: apregier <aregier@genome.wustl.edu>
    Date:   Wed Jan 9 13:06:26 2013 -0600
    
        Update source to 0.4.10.1
    
    commit 7713d9b42744c69f74b13f0f53b29e9f5f2d009d
    Author: apregier <aregier@genome.wustl.edu>
    Date:   Tue Jan 8 14:35:41 2013 -0600
    
        Copy more needed files
    
    commit 3cc0332b6b971712dca51b546f32527d4948113c
    Author: apregier <aregier@genome.wustl.edu>
    Date:   Tue Jan 8 14:08:28 2013 -0600
    
        No need for postinst or prerm
    
    commit 42669211baddb6b8d87bdbe543e4255478a0a5ea
    Author: apregier <aregier@genome.wustl.edu>
    Date:   Tue Jan 8 13:51:19 2013 -0600
    
        Copy necessary files into /usr/lib/
    
    commit 2d03f7e1523794adb4b5b870d3b56dbddddc534c
    Author: apregier <aregier@genome.wustl.edu>
    Date:   Mon Jan 7 16:39:34 2013 -0600
    
        Fixing debian bugs
    
    commit 854e1de7d8f56a8c270e3af96b205fbec852e510
    Author: apregier <aregier@genome.wustl.edu>
    Date:   Mon Jan 7 16:26:29 2013 -0600
    
        Add debian files
    
    commit 3375d3e8ab7e6967b170702adae11cf7c88bc485
    Author: apregier <aregier@genome.wustl.edu>
    Date:   Mon Jan 7 15:25:44 2013 -0600
    
        Initial version of source code
    
    commit 8cf7af9224574a5f4086525b8150b4c4a89f8647
    Author: apregier <aregier@genome.wustl.edu>
    Date:   Mon Jan 7 14:54:37 2013 -0600
    
        first commit

---
+++ strelka/lib/starling_common/indel.hh
@@ -0,0 +1,51 @@
+// -*- mode: c++; indent-tabs-mode: nil; -*-
+//
+// Copyright (c) 2009-2012 Illumina, Inc.
+//
+// This software is covered by the "Illumina Genome Analyzer Software
+// License Agreement" and the "Illumina Source Code License Agreement",
+// and certain third party copyright/licenses, and any user of this
+// source file is bound by the terms therein (see accompanying files
+// Illumina_Genome_Analyzer_Software_License_Agreement.pdf and
+// Illumina_Source_Code_License_Agreement.pdf and third party
+// copyright/license notices).
+//
+//
+
+/// \file
+///
+/// \author Chris Saunders
+///
+
+#ifndef __INDEL_HH
+#define __INDEL_HH
+
+#include "starling_common/indel_key.hh"
+#include "starling_common/indel_data.hh"
+
+#include <iosfwd>
+
+
+
+// Represents a single indel observation
+struct indel_observation {
+    indel_key key;
+    indel_observation_data data;
+};
+
+
+
+// Represents all information about an indel
+struct indel {
+    indel_key key;
+    indel_data data;
+};
+
+
+
+// Debugging dump:
+std::ostream& operator<<(std::ostream& os, const indel& in);
+
+
+
+#endif
